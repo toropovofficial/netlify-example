8f74cab7d920b7ab5432d1fddb485441c443db6e
toropov.a <toropov.a@roistat.com>
2023-03-12 19:19:15 +0300

    fixed bugs

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const path = require('path')
 
 const app = express();
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/dist')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile("index.html", { root: __dirname + "/dist"});
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
